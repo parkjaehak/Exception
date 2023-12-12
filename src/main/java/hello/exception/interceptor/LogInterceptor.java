27876f70f53541ed93b84ae2bcb323c78dd2d74c
parkjaehak <atom8426@naver.com>
2023-12-12 20:52:04 +0900

    3nd spring이 제공하는 ExceptionResolver

--- src/main/java/hello/exception/interceptor/LogInterceptor.java
+++ src/main/java/hello/exception/interceptor/LogInterceptor.java
@@ -29,7 +29,7 @@ public class LogInterceptor implements HandlerInterceptor {
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         String requestURI = request.getRequestURI();
-        String logId = (String)request.getAttribute(LOG_ID);
+        String logId = (String) request.getAttribute(LOG_ID);
 
         log.info("RESPONSE [{}][{}][{}]", logId, request.getDispatcherType(), requestURI);
 
